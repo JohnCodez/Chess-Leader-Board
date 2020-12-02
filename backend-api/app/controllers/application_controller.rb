5782343797720bf0bc8bba0383d7a14839788e45
JohnCodez <exarhouleasjohn@gmail.com>
2020-12-01 19:27:50 -0500

    Working to Mvp Backend

--- backend-api/app/controllers/application_controller.rb
+++ backend-api/app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::API
     
-    def index
-        gameplayers = Gameplayer.all
-        render json: gameplayers, include: [:player1_id, :player2_id, :game_id]
-    end 
+    # def index
+    #     gameplayers = Gameplayer.all
+    #     render json: gameplayers, include: [:player1_id, :player2_id, :game_id]
+    # end 
 
 end
--- backend-api/app/controllers/players_controller.rb
+++ backend-api/app/controllers/players_controller.rb
@@ -2,5 +2,5 @@ class PlayersController < ApplicationController
     def index
         @players = Player.all
         render json: @players
-      end
+    end
 end
