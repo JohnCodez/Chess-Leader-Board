399e55ed784e865f6d3cd74adeb79d21a860ed17
JohnCodez <exarhouleasjohn@gmail.com>
2020-12-02 14:28:53 -0500

    Wed Backend

--- backend-api/app/controllers/players_controller.rb
+++ backend-api/app/controllers/players_controller.rb
@@ -3,4 +3,18 @@ class PlayersController < ApplicationController
         @players = Player.all
         render json: @players
     end
+
+    def new
+        @player = Player.new
+    end
+
+    def create
+        @player = Player.create(player_params)
+    end
+
+    private
+
+    def player_params
+        params.require(:player).permit(:name, :image_url, :rank)
+    end
 end
