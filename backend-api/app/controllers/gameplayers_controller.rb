ae0df3e2dec12255bf4b3431b4c98b22b1f4a3e1
JohnCodez <exarhouleasjohn@gmail.com>
2020-12-03 17:14:35 -0500

    3 tables functionality

--- backend-api/app/controllers/gameplayers_controller.rb
+++ backend-api/app/controllers/gameplayers_controller.rb
@@ -3,4 +3,31 @@ class GameplayersController < ApplicationController
         @gameplayers = Gameplayer.all
         render json: @gameplayers
       end
+
+    def show
+        @gameplayer = Gameplayer.find(params[:id])
+        render json: @gameplayer
+    end
+
+    def new
+        @gameplayer = Gameplayer.new
+        render json: @gameplayer
+    end
+
+    def create
+        @gameplayer = Gameplayer.create(gameplayer_params)
+        render json: @gameplayer
+    end
+
+    def update
+        @gameplayer = Gameplayer.find(params[:id])
+        @gameplayer.update(gameplayer_params)
+        render json: @gameplayer
+    end
+
+    private
+
+    def gameplayer_params
+        params.require(:gameplayer).permit(:player1_id, :player2_id, :game_id )
+    end
 end
--- backend-api/app/controllers/games_controller.rb
+++ backend-api/app/controllers/games_controller.rb
@@ -2,5 +2,32 @@ class GamesController < ApplicationController
     def index
         @games = Game.all
         render json: @games
-      end
+    end
+
+    def show
+        @game = Game.find(params[:id])
+        render json: @game
+    end
+
+    def new
+        @game = Game.new
+        render json: @games
+    end
+
+    def create
+        @game = Game.create(game_params)
+        render json: @games
+    end
+
+    def update
+        @game = Game.find(params[:id])
+        @game.update(game_params)
+        render json: @game
+    end
+
+    private
+
+    def game_params
+        params.require(:game).permit(:winner_id)
+    end
 end
