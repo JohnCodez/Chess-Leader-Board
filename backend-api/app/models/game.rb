de4318a338c4fc708e51c40d6d60856365b98edb
jonnyhak <jonnyhak@gmail.com>
2020-12-06 15:36:48 -0500

    location persisted to backend

--- backend-api/app/models/game.rb
+++ backend-api/app/models/game.rb
@@ -2,4 +2,7 @@ class Game < ApplicationRecord
 has_many :gameplayers
 has_many :players, through: :gameplayers
 
+geocoded_by :location
+after_validation :geocode
+
 end
--- backend-api/db/seeds.rb
+++ backend-api/db/seeds.rb
@@ -15,9 +15,9 @@ bobby = Player.create(name: "Bobby Fischer", rank: 800, image_url: "https://cdn.
 magnus = Player.create(name: "Magnus Carlsen", rank: 900, image_url: "https://i.guim.co.uk/img/media/80c910214dbf4de7b660b09829b5348eefdb1d18/0_182_5472_3283/master/5472.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=69297966046d1f502d29e76ba78934ac")
 beth = Player.create(name: "Beth Harmon", rank: 1100, image_url: "https://www.indiewire.com/wp-content/uploads/2020/08/preview.jpg?w=780")
 
-game1 = Game.create(winner_id: bobby.id)
-game2 = Game.create(winner_id: magnus.id)
-game3 = Game.create(winner_id: beth.id)
+game1 = Game.create(winner_id: bobby.id, location: "Washington Square, New York, NY 10012" )
+game2 = Game.create(winner_id: magnus.id, location: "Center Drive, New York, NY 10019" )
+game3 = Game.create(winner_id: beth.id, location: "Bryant Park, W 40th St, New York, NY 10018")
 
 gameplayer1 = Gameplayer.create!(player1_id: bobby.id, player2_id: magnus.id, game_id: game1.id)
 gameplayer2 = Gameplayer.create!(player1_id: bobby.id, player2_id: magnus.id, game_id: game2.id)
