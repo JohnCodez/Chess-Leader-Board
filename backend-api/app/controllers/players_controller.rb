fd5b62bdc787163231ebd5af633d6590bd273a6f
JohnCodez <exarhouleasjohn@gmail.com>
2020-12-07 11:53:46 -0500

    new Player and Updating leaderboard works

--- backend-api/app/controllers/players_controller.rb
+++ backend-api/app/controllers/players_controller.rb
@@ -11,10 +11,12 @@ class PlayersController < ApplicationController
 
     def new
         @player = Player.new
+        render json: @player
     end
 
     def create
         @player = Player.create(player_params)
+        render json: @player
     end
 
     def update
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -76,167 +76,167 @@ const renderPlayers = (playersObj) => {
     
 }
 
-const renderNewPlayer = (newPlayerObj) => {
-    const li = document.createElement("li")
-    li.innerHTML = `         
-    <img src=${newPlayerObj.image_url} />
-    ${newPlayerObj.name} (${newPlayerObj.rank})
-    `
-    li.id = newPlayerObj.id
+// const renderNewPlayer = (newPlayerObj) => {
+//     const li = document.createElement("li")
+//     li.innerHTML = `         
+//     <img src=${newPlayerObj.image_url} />
+//     ${newPlayerObj.name} (${newPlayerObj.rank})
+//     `
+//     li.id = newPlayerObj.id
     
-    leaderBoardList.append(li)
+//     leaderBoardList.append(li)
     
-    const option1 = document.createElement("option")
-    option1.value = newPlayerObj.id
-    option1.dataset.id = newPlayerObj.rank
+//     const option1 = document.createElement("option")
+//     option1.value = newPlayerObj.id
+//     option1.dataset.id = newPlayerObj.rank
     
-    option1.textContent = `${newPlayerObj.name}`;
-    player1.append(option1)
+//     option1.textContent = `${newPlayerObj.name}`;
+//     player1.append(option1)
     
-    const option2 = document.createElement("option")
-    option2.value = newPlayerObj.id
-    option2.dataset.id = newPlayerObj.rank
-    option2.textContent = `${newPlayerObj.name}`;
-    player2.append(option2)
+//     const option2 = document.createElement("option")
+//     option2.value = newPlayerObj.id
+//     option2.dataset.id = newPlayerObj.rank
+//     option2.textContent = `${newPlayerObj.name}`;
+//     player2.append(option2)
     
-    main.addEventListener('change', (event) => {
-        let img = newPlayerObj.image_url
+//     main.addEventListener('change', (event) => {
+//         let img = newPlayerObj.image_url
         
-        if (event.target.id === 'choose-player-1'){
-            player1Img.src = img
-        } else if (event.target.id === 'choose-player-2') {
-            player2Img.src = img
-        }
-    })
-}         
+//         if (event.target.id === 'choose-player-1'){
+//             player1Img.src = img
+//         } else if (event.target.id === 'choose-player-2') {
+//             player2Img.src = img
+//         }
+//     })
+// }         
                 
-    // Event Handlers
+// Event Handlers
 
-    newPlayerForm.addEventListener('submit', (event) => {
-        event.preventDefault()
-        
-        const newPlayerObj = {
-            name: newPlayerForm.name.value,
-            image_url: newPlayerForm.image.value,
-            rank: getRandomNumber(700,1100)
-        }
-        
-        createPlayer(newPlayerObj)
-        newPlayerForm.reset()
-    })
+newPlayerForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    
+    const newPlayerObj = {
+        name: newPlayerForm.name.value,
+        image_url: newPlayerForm.image.value,
+        rank: getRandomNumber(700,1100)
+    }
+    
+    createPlayer(newPlayerObj)
+    newPlayerForm.reset()
+})
 
-    gameplayForm.addEventListener('submit', (event) => {
-        event.preventDefault()
-        winner = getRandomNumber(1,2)
-        const player1Id = player1.value 
-        const player2Id = player2.value 
-        betPlayer1 = parseInt(bet1.value)
-        betPlayer2 = parseInt(bet2.value)
-        h1Winner.textContent = "... BATTLING ..."
-        h1Winner.style.color = 'red';
-        setTimeout (function() {
+gameplayForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    winner = getRandomNumber(1,2)
+    const player1Id = player1.value 
+    const player2Id = player2.value 
+    betPlayer1 = parseInt(bet1.value)
+    betPlayer2 = parseInt(bet2.value)
+    h1Winner.textContent = "... BATTLING ..."
+    h1Winner.style.color = 'red';
+    setTimeout (function() {
+    
+    
+    fetch(`http://localhost:3000/players/${player1Id}`)
+    .then(response => response.json())
+    .then((player1Obj) => {
         
         
-        fetch(`http://localhost:3000/players/${player1Id}`)
-        .then(response => response.json())
-        .then((player1Obj) => {
-            
-            
-            //1st player
-            const playersIds = [player1Id, player2Id]
-            if (winner == 1){
-                let player1Rank = player1Obj.rank
-                const playerObj = {
-                    rank: player1Rank + 50
-                }  
-                updatePlayer(player1Id, playerObj, player1Id, player2Id, 1)
+        //1st player
+        const playersIds = [player1Id, player2Id]
+        if (winner == 1){
+            let player1Rank = player1Obj.rank
+            const playerObj = {
+                rank: player1Rank + 50
+            }  
+            updatePlayer(player1Id, playerObj, player1Id, player2Id, 1)
+        let li = document.getElementById(`${player1Id}`)
+        li.childNodes[3].textContent = playerObj.rank
+        winnerId = player1Id
+        h1Winner.textContent = player1Obj.name + ' Wins!'
+        h1Winner.style.color = 'green'
+
+        map.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBG9caRGRSzZCc1Wvfh5bvl-FoS0PNlWxY&q=${gameLocation.value}`
+        makeGame(winnerId, playersIds, gameLocation)
+        getBet(currentuser, betPlayer1)
+        
+    } else {
+        let player1Rank = player1Obj.rank
+        if (player1Rank >= 50) { 
+            const playerObj = {
+                rank: player1Rank - 50
+            }
+            updatePlayer(player1Id, playerObj, player1Id, player2Id, 0)
             let li = document.getElementById(`${player1Id}`)
             li.childNodes[3].textContent = playerObj.rank
-            winnerId = player1Id
-            h1Winner.textContent = player1Obj.name + ' Wins!'
-            h1Winner.style.color = 'green'
-
-            map.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBG9caRGRSzZCc1Wvfh5bvl-FoS0PNlWxY&q=${gameLocation.value}`
-            makeGame(winnerId, playersIds, gameLocation)
+            betPlayer1 = 0 - betPlayer1
             getBet(currentuser, betPlayer1)
             
         } else {
-            let player1Rank = player1Obj.rank
-            if (player1Rank >= 50) { 
-                const playerObj = {
-                    rank: player1Rank - 50
-                }
-                updatePlayer(player1Id, playerObj, player1Id, player2Id, 0)
-                let li = document.getElementById(`${player1Id}`)
-                li.childNodes[3].textContent = playerObj.rank
-                betPlayer1 = 0 - betPlayer1
-                getBet(currentuser, betPlayer1)
-                
-            } else {
-                const playerObj = {
-                    rank: 0
-                }
-                updatePlayer(player1Id, playerObj, player1Id, player2Id, 0)
-                let li = document.getElementById(`${player1Id}`)
-                li.childNodes[3].textContent = playerObj.rank
-                betPlayer1 = 0 - betPlayer1
-                getBet(currentuser, betPlayer1)
-            }
-        }
-        
-    }) 
-    
-    //2nd
-    fetch(`http://localhost:3000/players/${player2Id}`)
-    .then(response => response.json())
-    .then((player2Obj) => {
-        const playersIds = [player1Id, player2Id]
-        if (winner == 2) {
-            let player2Rank = player2Obj.rank
             const playerObj = {
-                rank: player2Rank + 50
-            }  
-            updatePlayer(player2Id, playerObj, player2Id, player1Id, 1)
+                rank: 0
+            }
+            updatePlayer(player1Id, playerObj, player1Id, player2Id, 0)
+            let li = document.getElementById(`${player1Id}`)
+            li.childNodes[3].textContent = playerObj.rank
+            betPlayer1 = 0 - betPlayer1
+            getBet(currentuser, betPlayer1)
+        }
+    }
+    
+}) 
+
+//2nd
+fetch(`http://localhost:3000/players/${player2Id}`)
+.then(response => response.json())
+.then((player2Obj) => {
+    const playersIds = [player1Id, player2Id]
+    if (winner == 2) {
+        let player2Rank = player2Obj.rank
+        const playerObj = {
+            rank: player2Rank + 50
+        }  
+        updatePlayer(player2Id, playerObj, player2Id, player1Id, 1)
+        let li = document.getElementById(`${player2Id}`)
+        li.childNodes[3].textContent = playerObj.rank
+        winnerId = player2Id
+        h1Winner.textContent = player2Obj.name + ' Wins!'
+        h1Winner.style.color = 'green'
+        // console.log(winnerId)
+        map.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBG9caRGRSzZCc1Wvfh5bvl-FoS0PNlWxY&q=${gameLocation.value}`
+        makeGame(winnerId, playersIds, gameLocation)
+        getBet(currentuser, betPlayer2)
+    } else {
+        let player2Rank = player2Obj.rank
+        if (player2Rank >= 50) { 
+            const playerObj = {
+                rank: player2Rank - 50
+            }
+            updatePlayer(player2Id, playerObj, player1Id, player2Id, 0)
             let li = document.getElementById(`${player2Id}`)
             li.childNodes[3].textContent = playerObj.rank
-            winnerId = player2Id
-            h1Winner.textContent = player2Obj.name + ' Wins!'
-            h1Winner.style.color = 'green'
-            // console.log(winnerId)
-            map.src = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBG9caRGRSzZCc1Wvfh5bvl-FoS0PNlWxY&q=${gameLocation.value}`
-            makeGame(winnerId, playersIds, gameLocation)
+            betPlayer2 = 0 - betPlayer2
             getBet(currentuser, betPlayer2)
+            
+            
         } else {
-            let player2Rank = player2Obj.rank
-            if (player2Rank >= 50) { 
-                const playerObj = {
-                    rank: player2Rank - 50
-                }
-                updatePlayer(player2Id, playerObj, player1Id, player2Id, 0)
-                let li = document.getElementById(`${player2Id}`)
-                li.childNodes[3].textContent = playerObj.rank
-                betPlayer2 = 0 - betPlayer2
-                getBet(currentuser, betPlayer2)
-                
-                
-            } else {
-                const playerObj = {
-                    rank: 0
-                }
-                updatePlayer(player2Id, playerObj, player1Id, player2Id, 0)
-                let li = document.getElementById(`${player2Id}`)
-                li.childNodes[3].textContent = playerObj.rank
-                betPlayer2 = 0 - betPlayer2
-                getBet(currentuser, betPlayer2)
-                
+            const playerObj = {
+                rank: 0
             }
-        }})
-        
-        bet1.disabled = false
-        bet2.disabled = false
-        
-    }, 3000)
-    })
+            updatePlayer(player2Id, playerObj, player1Id, player2Id, 0)
+            let li = document.getElementById(`${player2Id}`)
+            li.childNodes[3].textContent = playerObj.rank
+            betPlayer2 = 0 - betPlayer2
+            getBet(currentuser, betPlayer2)
+            
+        }
+    }})
+    
+    bet1.disabled = false
+    bet2.disabled = false
+    
+}, 3000)
+})
 
 reset.addEventListener("reset", (event) => {
     event.preventDefault()
@@ -261,7 +261,14 @@ const createPlayer = (newPlayerObj) => {
         .then(response => {
             return response.json()
         })
-        .then(renderNewPlayer(newPlayerObj))
+        .then(() => {
+            player1.options.length = 0
+            player2.options.length = 0
+            while (leaderBoardList.firstChild) {
+                leaderBoardList.removeChild(leaderBoardList.lastChild)
+            }                   
+            getPlayers()
+        })
 }
 
 const updatePlayer = (id, playerObj, player1Id, player2Id, statusRun) => {
@@ -276,6 +283,12 @@ const updatePlayer = (id, playerObj, player1Id, player2Id, statusRun) => {
         .then(() => {
             if (statusRun === 1) {
                 getPlayer(player1Id, player2Id)
+                player1.options.length = 0
+                player2.options.length = 0
+                while (leaderBoardList.firstChild) {
+                    leaderBoardList.removeChild(leaderBoardList.lastChild)
+                }                   
+                getPlayers()
             }
             })
 }
@@ -302,4 +315,11 @@ function getRandomNumber(min, max) {
     }
   }
 getPlayers()
+
+// while (player1.firstElementChild) {
+//     player1.removeChild(player1.lastElementChild)
+// }
+// while (player2.firstElementChild) {
+//     player2.removeChild(player2.lastElementChild)
+// }
 
